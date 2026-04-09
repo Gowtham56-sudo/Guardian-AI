f1fdae713eb8d681907a177703b87a2ab01c7fb7
Gowtham T <gowtham.t20062@gmail.com>
2026-04-09 22:03:07 +0530

    update 7

--- api/sos/alert.ts
+++ api/sos/alert.ts
@@ -1,4 +1,4 @@
-import { processSosAlert, type SosMessageInput } from "../../lib/sos";
+import { processSosAlert, type SosMessageInput } from "./sos-lib";
 
 export default async function handler(req: any, res: any) {
   if (req.method !== "POST") {
